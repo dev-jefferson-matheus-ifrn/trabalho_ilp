60d7e86f81f1a4890d7b29c4cec4308d9114f40b
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-07-20 21:41:32 -0300

    cadastro de horarios e links na pagina principal

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from routes.router import home_blue_print, cadastro_especialidade_blue_print, cadastro_medicos_blue_print
+from routes.router import home_blue_print, cadastro_especialidade_blue_print, cadastro_medicos_blue_print, cadastro_horarios_blue_print
 
 app = Flask(__name__)
 app.register_blueprint(home_blue_print)
 app.register_blueprint(cadastro_especialidade_blue_print)
 app.register_blueprint(cadastro_medicos_blue_print)
+app.register_blueprint(cadastro_horarios_blue_print)
 
 if __name__ == "__main__":
     app.run()
--- routes/router.py
+++ routes/router.py
@@ -1,13 +1,16 @@
 from flask import render_template, Blueprint, request
 import csv
+
 caminho_tabela_especialidades = "db_tables/tb_especialidades.csv"
 caminho_tabela_medicos = "db_tables/tb_medicos.csv"
+caminho_tabela_horarios = "db_tables/tb_horatios.csv"
 
 
 home_blue_print = Blueprint("home", __name__ )
 cadastro_especialidade_blue_print = Blueprint("cadastro_especialidade", __name__ )
 home_blue_print = Blueprint("home", __name__ )
 cadastro_medicos_blue_print = Blueprint("cadastro_medicos", __name__ )
+cadastro_horarios_blue_print = Blueprint("cadastro_horarios", __name__ )
 
 @home_blue_print.route("/")
 def home_page():
@@ -42,3 +45,21 @@ def cadastrar_medico():
 
 
     return render_template("cadastro_medicos.html", especialidades=especialidades)
+
+
+@cadastro_horarios_blue_print.route("/cadastro_horarios", methods=["GET", "POST"])
+def cadastrar_horarios_pagina():
+    with open(caminho_tabela_medicos, "r", newline="", encoding="utf-8") as arquivo:
+        medicos = []
+        for linha in csv.reader(arquivo):
+            if linha[0] != "especialidades":
+                medicos.append(linha[0])
+
+        if request.method == "POST":
+            horario = request.form.get("tempo")
+            medico = request.form.get("medicos")
+            with open(caminho_tabela_horarios, "a", newline= "", encoding= "utf-8") as arquivo:
+                escritor = csv.writer(arquivo)
+                escritor.writerow([horario,medico])
+
+    return render_template("cadastro_horarios_medicos.html", medicos=medicos)
