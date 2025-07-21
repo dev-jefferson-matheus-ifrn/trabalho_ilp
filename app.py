9febe3674d42d436cb0daaaeaad95644a1ed39fd
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2025-07-21 11:27:08 -0300

    funcionalidade de deletar especialidade

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from routes.router import home_blue_print, cadastro_especialidade_blue_print, cadastro_medicos_blue_print, cadastro_horarios_blue_print
+from routes.router import home_blue_print, cadastro_especialidade_blue_print, cadastro_medicos_blue_print, cadastro_horarios_blue_print, listar_especialidades_blue_print,deletar_especialidade_blue_print
 
 app = Flask(__name__)
 app.register_blueprint(home_blue_print)
 app.register_blueprint(cadastro_especialidade_blue_print)
 app.register_blueprint(cadastro_medicos_blue_print)
 app.register_blueprint(cadastro_horarios_blue_print)
+app.register_blueprint(listar_especialidades_blue_print)
+app.register_blueprint(deletar_especialidade_blue_print)
 
 if __name__ == "__main__":
     app.run()
--- routes/router.py
+++ routes/router.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, request
+from flask import render_template, Blueprint, request, redirect, url_for
 import csv
 
 caminho_tabela_especialidades = "db_tables/tb_especialidades.csv"
@@ -11,6 +11,8 @@ cadastro_especialidade_blue_print = Blueprint("cadastro_especialidade", __name__
 home_blue_print = Blueprint("home", __name__ )
 cadastro_medicos_blue_print = Blueprint("cadastro_medicos", __name__ )
 cadastro_horarios_blue_print = Blueprint("cadastro_horarios", __name__ )
+listar_especialidades_blue_print = Blueprint("listar_especialidades", __name__ )
+deletar_especialidade_blue_print = Blueprint("deletar_especialidade", __name__ )
 
 @home_blue_print.route("/")
 def home_page():
@@ -22,7 +24,7 @@ def cadastro_especialidade_page():
     if request.method == "POST":
         especialidade = request.form.get("especialidade")
         with open(caminho_tabela_especialidades, "a", newline= "") as arquivo:
-            escritor = csv.writer(arquivo)
+            escritor = csv.writer(arquivo,delimiter=",")
             escritor.writerow([especialidade])
     return render_template("cadastro_especialidade.html")
 
@@ -51,9 +53,12 @@ def cadastrar_medico():
 def cadastrar_horarios_pagina():
     with open(caminho_tabela_medicos, "r", newline="", encoding="utf-8") as arquivo:
         medicos = []
-        for linha in csv.reader(arquivo):
-            if linha[0] != "especialidades":
-                medicos.append(linha[0])
+        csv_reader = csv.reader(arquivo)
+
+        next(csv_reader)
+
+        for linha in csv_reader:
+            medicos.append(linha[0])
 
         if request.method == "POST":
             horario = request.form.get("tempo")
@@ -63,3 +68,52 @@ def cadastrar_horarios_pagina():
                 escritor.writerow([horario,medico])
 
     return render_template("cadastro_horarios_medicos.html", medicos=medicos)
+
+
+@listar_especialidades_blue_print.route("/listar_especialidades" , methods = ["GET", "POST"])
+def listar_especialidades_pagina():
+    with open(caminho_tabela_especialidades, "r", newline="", encoding="utf-8") as arquivo:
+        especialidades = []
+        csv_reader = csv.reader(arquivo)
+
+        next(csv_reader)
+
+        for linha in csv_reader:
+            especialidades.append(linha[0])
+
+    return render_template("lista_especialidades.html", especialidades=especialidades)
+
+
+@deletar_especialidade_blue_print.route("/deletar_especialidade", methods = ["GET", "POST"])
+def deletar_especialidade_pagina():
+    with open(caminho_tabela_especialidades, "r", newline="", encoding="utf-8") as arquivo:
+        especialidades = []
+        csv_reader = csv.reader(arquivo)
+
+        
+
+        for linha in csv_reader:
+            if linha[0] != "especialidades":
+                especialidades.append(linha[0])
+    
+    return render_template("deletar_especialidade.html", especialidades=especialidades)
+
+
+@deletar_especialidade_blue_print.route("/deletar_especialidade/<nome>", methods = ["POST"])
+def deletar_especialidade(nome):
+    with open(caminho_tabela_especialidades, "r", newline="", encoding="utf-8") as arquivo:
+        especialidades = []
+        csv_reader = csv.reader(arquivo)
+
+        filds = next(csv_reader)
+
+        for linha in csv_reader:
+            if linha[0] != nome:
+                especialidades.append(linha[0])
+    
+    with open(caminho_tabela_especialidades, "w", newline="", encoding="utf-8") as arquivo:
+        csv_writer = csv.writer(arquivo, delimiter=',')
+        csv_writer.writerow(filds)
+        csv_writer.writerow(especialidades)
+
+    return redirect(url_for("deletar_especialidade.deletar_especialidade_pagina"))
